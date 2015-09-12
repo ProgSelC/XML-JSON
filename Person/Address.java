8a88072d034fdd3d8719dac4a094c7b962a8e8ca
ProgSelC <ProgSelC@users.noreply.github.com>
2015-09-12 16:40:47 +0300

    Create Address.java

---
+++ Person/Address.java
@@ -0,0 +1,14 @@
+public class Address {
+    String country;
+    String city;
+    String street;
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
